8939fe08a707330d07632ee005778c7208900655
never-better <youtaktak@gmail.com>
2023-03-01 23:30:50 +0900

    feat: Home Navigation 구현
    각 화면 별 이동 구현

--- kukuku/kukuku/Presentation/Home/ViewController/HomeViewController.swift
+++ kukuku/kukuku/Presentation/Home/ViewController/HomeViewController.swift
@@ -5,6 +5,7 @@
 //  Created by youtak on 2023/03/01.
 //
 
+import Combine
 import UIKit
 
 import FlexLayout
@@ -19,6 +20,8 @@ class HomeViewController: UIViewController {
         return view
     }
 
+    private var cancellable = Set<AnyCancellable>()
+
     override func loadView() {
         view = HomeView()
     }
@@ -26,6 +29,7 @@ class HomeViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         configureNavigationBar()
+        subscribeButtonPublisher()
     }
 
     private func configureNavigationBar() {
@@ -33,5 +37,38 @@ class HomeViewController: UIViewController {
         label.textColor = .dynamicBlack
         label.text = "쿠쿠쿠"
         navigationItem.leftBarButtonItem = UIBarButtonItem(customView: label)
+    }
+
+    // MARK: - Navigation
+
+    private func subscribeButtonPublisher() {
+        homeView.konkukInfoListButtonPublisher()
+            .sink { [weak self] _ in
+                let konkukInfoListViewController = KonkukInfoListViewController()
+                self?.navigationController?.pushViewController(konkukInfoListViewController, animated: true)
+            }
+            .store(in: &cancellable)
+
+        homeView.guideButtonPublisher()
+            .sink { [weak self] _ in
+                let guideViewController = GuideViewController()
+                self?.navigationController?.pushViewController(guideViewController, animated: true)
+            }
+            .store(in: &cancellable)
+
+        homeView.settingButtonPublisher()
+            .sink { [weak self] _ in
+                let settingViewController = SettingViewController()
+                self?.navigationController?.pushViewController(settingViewController, animated: true)
+            }
+            .store(in: &cancellable)
+
+        homeView.arButtonPublisher()
+            .sink { [weak self] _ in
+                let arGameViewController = ARGameViewController()
+                arGameViewController.modalPresentationStyle = .fullScreen
+                self?.present(arGameViewController, animated: true)
+            }
+            .store(in: &cancellable)
     }
 }
--- kukuku/kukuku/Presentation/Setting/ViewController/SettingViewController.swift
+++ kukuku/kukuku/Presentation/Setting/ViewController/SettingViewController.swift
@@ -5,4 +5,13 @@
 //  Created by youtak on 2023/03/01.
 //
 
-import Foundation
+import UIKit
+
+final class SettingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .background
+        print("설정")
+    }
+}
