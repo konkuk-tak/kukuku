679c0e346ee35e08a169fa74ba66a0a2324eddff
never-better <youtaktak@gmail.com>
2023-03-01 19:50:20 +0900

    feat: HomeView 기본 flexlayout 설정

--- kukuku/kukuku/Presentation/Home/View/HomeView.swift
+++ kukuku/kukuku/Presentation/Home/View/HomeView.swift
@@ -9,17 +9,45 @@ import UIKit
 
 class HomeView: UIView {
     
+    private let containerView = UIView()
+    private let titleLabel = UILabel()
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         configureHomeView()
+        configureSubViews()
+        configureFlexLayout()
     }
 
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    private func configureHomeView() {
-        backgroundColor = .green
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        containerView.pin.all(pin.safeArea)
+        containerView.flex.layout(mode: .adjustHeight)
+        configureFlexLayout()
     }
     
+    private func configureHomeView() {
+        backgroundColor = .background
+    }
+    
+    private func configureSubViews() {
+        addSubview(containerView)
+        configureTitleLabel()
+    }
+    
+    private func configureTitleLabel() {
+        titleLabel.text = "지금까지 먹은 햄버거"
+        titleLabel.textColor = .dynamicBlack
+        titleLabel.font = .bitText
+    }
+    
+    private func configureFlexLayout() {
+        containerView.flex.direction(.column).define { flex in
+            flex.addItem(titleLabel)
+        }
+    }
 }
