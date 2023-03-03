c547c73a873cf73f7fa522bed4675f61f50f9620
never-better <youtaktak@gmail.com>
2023-03-03 15:43:39 +0900

    feat: KeyChainManager 구현
    - data 타입 저장
    - 싱글턴으로 선언

---
+++ kukuku/kukuku/Data/Manager/KeyChainManager.swift
@@ -0,0 +1,96 @@
+//
+//  KeyChainManager.swift
+//  kukuku
+//
+//  Created by youtak on 2023/03/03.
+//
+
+import Foundation
+
+final class KeyChainManger {
+
+    static let main = KeyChainManger()
+    private init() {}
+
+    private let account = "userAccount"
+
+    private func createQuery(data: Data) -> CFDictionary {
+        return [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: account,
+            kSecValueData: data
+        ] as CFDictionary
+    }
+
+    private func readQuery() -> CFDictionary {
+        return [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: account,
+            kSecMatchLimit: kSecMatchLimitOne,
+            kSecReturnAttributes: true,
+            kSecReturnData: true
+        ] as CFDictionary
+    }
+
+    private func updateQuery() -> CFDictionary {
+        return [
+            kSecClass: kSecClassGenericPassword,
+            kSecAttrAccount: account
+        ] as CFDictionary
+    }
+
+    private func updateAttributes(data: Data) -> CFDictionary {
+        return [
+            kSecAttrAccount: account,
+            kSecValueData: data
+        ] as CFDictionary
+    }
+
+    private func deleteQuery() -> CFDictionary {
+        return updateQuery()
+    }
+}
+
+extension KeyChainManger {
+    func createUser() throws {
+        let query = createQuery(data: Data())
+        let status = SecItemAdd(query, nil)
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+    }
+
+    func readUser() throws -> User {
+        let query = readQuery()
+        var typeRef: CFTypeRef?
+
+        if SecItemCopyMatching(query, &typeRef) != errSecSuccess {
+            throw KeychainError.noData
+        }
+
+        guard let typeRef = typeRef as? [CFString: Any],
+              let data = typeRef[kSecValueData] as? Data else {
+            throw KeychainError.noData
+        }
+
+        return User(type: .normal, score: 0, log: [])
+    }
+
+    func updateUser(user: User) throws {
+        let query = updateQuery()
+        let attributes = updateAttributes(data: Data())
+        let status = SecItemUpdate(query, attributes)
+        guard status != errSecItemNotFound else { throw KeychainError.noData }
+        guard status == errSecSuccess else { throw KeychainError.unhandledError(status: status) }
+    }
+
+    func deleteUser() throws {
+        let query = deleteQuery()
+        let status = SecItemDelete(query)
+        guard status == errSecSuccess || status == errSecItemNotFound else { throw KeychainError.unhandledError(status: status) }
+    }
+}
+
+enum KeychainError: Error {
+    case jsonParsing
+    case noData
+    case unhandledError(status: OSStatus)
+}
