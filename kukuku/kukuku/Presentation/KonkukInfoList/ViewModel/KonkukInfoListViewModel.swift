07323aa20da8f2f19df8709629cf8382ff2fc52e
never-better <youtaktak@gmail.com>
2023-03-04 11:38:35 +0900

    feat: Konkuk Info ViewModel 구현

--- kukuku/kukuku/Presentation/KonkukInfoList/ViewModel/KonkukInfoListViewModel.swift
+++ kukuku/kukuku/Presentation/KonkukInfoList/ViewModel/KonkukInfoListViewModel.swift
@@ -5,7 +5,33 @@
 //  Created by youtak on 2023/03/04.
 //
 
+import Combine
 import Foundation
 
 final class KonkukInfoListViewModel {
+
+    private var konkukInfoUseCase: KonkukInfoUseCase
+
+    init(konkukInfoUseCase: KonkukInfoUseCase) {
+        self.konkukInfoUseCase = konkukInfoUseCase
+    }
+
+    struct Input {
+        let viewDidLoad: AnyPublisher<Int, Never>
+    }
+
+    struct Output {
+        let infoList: AnyPublisher<[KonkukInfo]?, Never>
+    }
+
+    func transform(input: Input) -> Output {
+
+        let konkukInfoList = input.viewDidLoad
+            .map { [weak self] count in
+                self?.konkukInfoUseCase.infoList(count: count)
+            }
+            .eraseToAnyPublisher()
+
+        return Output(infoList: konkukInfoList)
+    }
 }
