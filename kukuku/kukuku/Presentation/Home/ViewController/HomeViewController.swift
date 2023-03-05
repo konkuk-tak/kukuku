096fe02623ecf312e513a614ad777e0a03d24e80
never-better <youtaktak@gmail.com>
2023-03-05 20:32:28 +0900

    feat: 더미 데이터 제거

--- kukuku/kukuku/Presentation/Home/ViewController/HomeViewController.swift
+++ kukuku/kukuku/Presentation/Home/ViewController/HomeViewController.swift
@@ -178,7 +178,7 @@ extension HomeViewController {
     // MARK: - Navigation
     private func moveToKonukInfoList() {
         let userListCount = homeViewModel.user.listCount
-        let konkukInfoListViewModel = DependencyFactory.konkukInfoListViewModel(userListCount: 30) // TODO: - 수정
+        let konkukInfoListViewModel = DependencyFactory.konkukInfoListViewModel(userListCount: userListCount)
         let konkukInfoListViewController = KonkukInfoListViewController(konkukInfoListViewModel: konkukInfoListViewModel)
         self.navigationController?.pushViewController(konkukInfoListViewController, animated: true)
     }
