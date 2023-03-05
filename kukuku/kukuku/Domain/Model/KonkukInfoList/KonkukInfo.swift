b59ea1ce0e6e3fd51cbd499985c1f89c539e28b3
never-better <youtaktak@gmail.com>
2023-03-05 17:57:36 +0900

    feat: KonkukInfo 모델 수정

--- kukuku/kukuku/Domain/Model/KonkukInfoList/KonkukInfo.swift
+++ kukuku/kukuku/Domain/Model/KonkukInfoList/KonkukInfo.swift
@@ -9,7 +9,16 @@ import Foundation
 
 struct KonkukInfo: Decodable {
     let id: String
-    let imageURL: String?
+    let imageURL: String
+    let imageReference: String
     let title: String
     let description: String
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case imageURL
+        case imageReference = "imageRef"
+        case title
+        case description
+    }
 }
