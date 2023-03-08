5cba450cc151f91d7a03234f08d1d876340f15ef
never-better <youtaktak@gmail.com>
2023-03-08 16:10:20 +0900

    feat: plist -> xcconfig로 수정

--- kukuku/kukuku/Data/Repository/DeveloperCodeRepository/DefaultDeveloperCodeRepository.swift
+++ kukuku/kukuku/Data/Repository/DeveloperCodeRepository/DefaultDeveloperCodeRepository.swift
@@ -9,20 +9,9 @@ import Foundation
 
 struct DefaultDeveloperCodeRepository: DeveloperCodeRepository {
 
-    private let fileName: String = "DeveloperCode"
-    private let key: String = "code"
+    private let key: String = "Developer Code"
 
     func code() -> String? {
-        guard let url = Bundle.main.url(forResource: fileName, withExtension: "plist") else {
-            return nil
-        }
-
-        guard let dictionary = NSDictionary(contentsOf: url) else {
-            return nil
-        }
-
-        let code = dictionary[key] as? String
-
-        return code
+        return Bundle.main.object(forInfoDictionaryKey: key) as? String
     }
 }
