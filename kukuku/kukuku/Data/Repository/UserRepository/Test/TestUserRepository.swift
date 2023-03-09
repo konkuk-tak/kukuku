04bcc085520df490f3d7983116db5068c32c1ded
never-better <youtaktak@gmail.com>
2023-03-09 20:31:37 +0900

    feat: configuration에 따른 user Repository 분기 처리

--- kukuku/kukuku/Data/Repository/UserRepository/Test/TestUserRepository.swift
+++ kukuku/kukuku/Data/Repository/UserRepository/Test/TestUserRepository.swift
@@ -8,14 +8,25 @@
 import Foundation
 
 final class TestUserRepository: UserRepository {
-    
+
     func createUser(user: User) throws {}
-    
+
     func readUser() throws -> User {
-        return User(type: .normal, listCount: 30, log: [])
+        return User(type: .normal, listCount: 30, log: createThirtyDaysLogs())
     }
-    
+
     func updateUser(user: User) throws {}
-    
+
     func deleteUser() throws {}
+
+    private func createThirtyDaysLogs() -> [Date] {
+        let standardValue = -32
+        var result: [Date] = []
+        for beforeDay in 0..<30 {
+            if let target = Calendar.current.date(byAdding: .day, value: standardValue + beforeDay, to: Date()) {
+                result.append(target)
+            }
+        }
+        return result
+    }
 }
