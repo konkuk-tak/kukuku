5b8d076b911f330ee00aef82aad5ef94a75428da
never-better <youtaktak@gmail.com>
2023-03-28 17:26:11 +0900

    feat: developer Code xcconfig -> plist

--- kukuku/kukuku/Data/Repository/DeveloperCodeRepository/DefaultDeveloperCodeRepository.swift
+++ kukuku/kukuku/Data/Repository/DeveloperCodeRepository/DefaultDeveloperCodeRepository.swift
@@ -9,9 +9,21 @@ import Foundation
 
 struct DefaultDeveloperCodeRepository: DeveloperCodeRepository {
 
-    private let key: String = "Developer Code"
+    private let key: String = "key"
 
     func code() -> String? {
-        return Bundle.main.object(forInfoDictionaryKey: key) as? String
+        guard let url = plistURL() else {
+            return nil
+        }
+
+        guard let dictionary = NSDictionary(contentsOf: url) else {
+            return nil
+        }
+
+        return dictionary["key"] as? String
+    }
+
+    private func plistURL() -> URL? {
+        return Bundle.main.url(forResource: "Developer-Code", withExtension: "plist")
     }
 }
